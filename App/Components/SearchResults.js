1bd00944ddf5aec71d1ed2247879166a58675b92
sanlouise <sandra.adamshallie@gmail.com>
2016-08-13 21:57:34 +0200

    Add SearchResults Component

---
+++ App/Components/SearchResults.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Image,
+  ListView,
+  TouchableHighlight
+} from 'react-native';
+
+class SearchResults extends Component {
+ 
+  constructor(props) {
+    super(props);
+    var dataSource = new ListView.DataSource(
+      {rowHasChanged: (r1, r2) => r1.guid !== r2.guid});
+    this.state = {
+      dataSource: dataSource.cloneWithRows(this.props.listings)
+    };
+  }
+ 
+  renderRow(rowData, sectionID, rowID) {
+    return (
+      <TouchableHighlight
+          underlayColor='#dddddd'>
+        <View>
+          <Text>{rowData.title}</Text>
+        </View>
+      </TouchableHighlight>
+    );
+  }
+ 
+  render() {
+    return (
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={this.renderRow.bind(this)}/>
+    );
+  }
+ 
+}
