1d1e4737494e2cae3d75b1e28f692edcba30eabe
sanlouise <sandra.adamshallie@gmail.com>
2016-08-13 19:04:21 +0200

    Add major styles and main functionality to Search Component

--- App/Components/Search.js
+++ App/Components/Search.js
@@ -7,11 +7,11 @@ import {
   View,
   TouchableHighlight,
   ActivityIndicatorIOS,
-  Image,
-  Component
+  Image
 } from 'react-native';
 
-var styles = StyleSheet.create({
+const styles = StyleSheet.create({
+
   description: {
     marginBottom: 20,
     fontSize: 18,
@@ -22,20 +22,85 @@ var styles = StyleSheet.create({
     padding: 30,
     marginTop: 65,
     alignItems: 'center'
+  },
+  flowRight: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    alignSelf: 'stretch'
+  },
+  buttonText: {
+    fontSize: 18,
+    color: 'white',
+    alignSelf: 'center'
+  },
+  button: {
+    height: 36,
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: '#48BBEC',
+    borderColor: '#48BBEC',
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 10,
+    alignSelf: 'stretch',
+    justifyContent: 'center'
+  },
+  searchInput: {
+    height: 36,
+    padding: 4,
+    marginRight: 5,
+    flex: 4,
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: '#48BBEC',
+    borderRadius: 8,
+    color: '#48BBEC'
   }
 });
 
 
 class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchString: 'london',
+      isLoading: false,
+      message: ''
+    };
+  }
+
   render() {
+    var spinner = this.state.isLoading ?
+    ( <ActivityIndicatorIOS
+        size='large'/> ) :
+    ( <View/>);
     return (
       <View style={styles.container}>
         <Text style={styles.description}>
           Search for houses to buy!
         </Text>
         <Text style={styles.description}>
-          Search by city, postcode or search near your location.
+          Search by place-name, postcode or search near your location.
         </Text>
+        <View style={styles.flowRight}>
+          <TextInput
+            style={styles.searchInput}
+            value={this.state.searchString}
+            placeholder='Search via name or postcode'/>
+          <TouchableHighlight style={styles.button}
+              underlayColor='#99d9f4'
+              >
+            <Text style={styles.buttonText}>Go</Text>
+          </TouchableHighlight>
+        </View>
+        <TouchableHighlight style={styles.button}
+            underlayColor='#99d9f4'
+            >
+          <Text style={styles.buttonText}>Location</Text>
+        </TouchableHighlight>
+        
+        {spinner}
+        <Text style={styles.description}>{this.state.message}</Text>
       </View>
     );
   }
