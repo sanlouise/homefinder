49454df0397158cad009999519b8c967f3361f70
sanlouise <sandra.adamshallie@gmail.com>
2016-08-13 22:02:34 +0200

    Add styles to SearchResults

--- App/Components/SearchResults.js
+++ App/Components/SearchResults.js
@@ -10,6 +10,34 @@ import {
   TouchableHighlight
 } from 'react-native';
 
+const styles = StyleSheet.create({
+  thumb: {
+    width: 80,
+    height: 80,
+    marginRight: 10
+  },
+  textContainer: {
+    flex: 1
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#dddddd'
+  },
+  price: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: '#48BBEC'
+  },
+  title: {
+    fontSize: 20,
+    color: '#656565'
+  },
+  rowContainer: {
+    flexDirection: 'row',
+    padding: 10
+  }
+});
+
 class SearchResults extends Component {
  
   constructor(props) {
